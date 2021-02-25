672cfa47685a268ff685ecfa1b75b70072e7d80d
Serhii Ponomarenko <ponomarenkoss99@gmail.com>
2021-02-25 14:06:20 +0200

    Add task1

--- src/com/p0n0marenk0/Main.java
+++ src/com/p0n0marenk0/Main.java
@@ -1,25 +1,23 @@
 //task2
-//find amount of lucky tickets on tram from 000001 to 999999
+//The digital clock shows the time in the format from 00:00 to 23:59.
+//Count how many times per day it happens that a symmetrical combination
+//is shown to the left of the colon for the one to the right of the colon
+//(for example, 02:20, 11:11 or 15:51).
 package com.p0n0marenk0;
 
 public class Main {
 
     public static void main(String[] args) {
         int count = 0;
-        for (int i = 0; i < 1000; i++) {
-            for (int j = 0; j < 1000; j++) {
-                String time = String.format("%03d%03d", i, j);
-                Integer number0 = Integer.valueOf(time.substring(0, 1));
-                Integer number1 = Integer.valueOf(time.substring(1, 2));
-                Integer number2 = Integer.valueOf(time.substring(2, 3));
-                Integer number3 = Integer.valueOf(time.substring(3, 4));
-                Integer number4 = Integer.valueOf(time.substring(4, 5));
-                Integer number5 = Integer.valueOf(time.substring(5));
-                if ((number0 + number1 + number2) == (number3 + number4 + number5)) {
+        for (int i = 0; i < 24; i++) {
+            for (int j = 0; j < 60; j++) {
+                String time = String.format("%02d:%02d", i, j);
+                if ((time.charAt(0) == time.charAt(4)) && (time.charAt(1) == time.charAt(3))) {
                     count++;
+                    System.out.println(time);
                 }
             }
         }
-        System.out.println("В пачке билетов было замечено " + count + " счастливых пассажира.");
+        System.out.println("Событий: " + count + " раз.");
     }
 }
